7630a16cf22555af6d525e77207abbdfd2206c2f
ktc <>
2026-01-31 17:59:37 +0900

    Dx12 swapchain(Window)

--- projectdo45/projectdo45/Dx12.h
+++ projectdo45/projectdo45/Dx12.h
@@ -16,7 +16,7 @@ public:
     IDXGIAdapter1* GetHardwareAdapter(IDXGIFactory4* factory);
     ID3D12Device* CreateD3D12Device(IDXGIAdapter1* adapter);
     ID3D12CommandQueue* CreateCommandQueue(ID3D12Device* device);
-    IDXGISwapChain3* CreateSwapChain(IDXGIFactory4* factory, ID3D12CommandQueue* commandQueue, HWND hwnd , Window window);
+    IDXGISwapChain3* CreateSwapChain(IDXGIFactory4* factory, ID3D12CommandQueue* commandQueue, HWND hwnd , Window );
     void                EnableDebugLayer();
     ID3D12Device* device;
     IDXGISwapChain3* swapChain;
